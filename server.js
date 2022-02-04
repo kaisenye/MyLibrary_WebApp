392f7991345d5aea25a247063d470c4aa4085857
Kaisen Ye <75810017+kaisenye@users.noreply.github.com>
2022-02-03 21:29:11 -0500

    setup MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+if (process.env.NODE_ENV != 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
-const expresLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts')
+
+
+const indexRouter = require('./routes/index')
 
 app.set('view engine', 'ejs')
-app.set('views', _dirname + '/views')
+app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static('public'))
+app.use(express.static('public')) 
 
 
+// setup mongoose
+const mongoose = require('mongoose')
+const req = require('express/lib/request')
+mongoose.connect(process.nextTick.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+
+
+// get router from index.js
+app.use('/', indexRouter)
+
+// run server
 app.listen(process.env.PORT || 3000)
