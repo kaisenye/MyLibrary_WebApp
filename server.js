7db35232c742f017527c3e869f36cf100fe680aa
Kaisen Ye <75810017+kaisenye@users.noreply.github.com>
2022-02-04 22:59:13 -0500

    author pag and add new author feature

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV != 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public')) 
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 
 // set up mongoose
@@ -23,10 +26,11 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-// , {useNewUrlParser: true}
 
-// get router from index.js
+
+// connect routers
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 // run server
 app.listen(process.env.PORT || 3000)
